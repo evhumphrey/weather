60fd5ee0ea8d377956ec39d97dcafce0efce1cfc
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-05-05 14:08:21 -0700

    Fixed Broken Commit #2

---
+++ widgets.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './root';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rooot = document.getElementById("root");
+  ReactDOM.render(<Root/>, rooot);
+});
